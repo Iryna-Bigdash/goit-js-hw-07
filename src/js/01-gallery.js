0d4164b2f52e8b821f494b4e9485491ff3f188cd
Iryna B <imbigdash@gmail.com>
2023-03-02 20:15:16 +0000

    added 1-st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryImageCard = createGalleriItems(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImageCard);
+galleryContainer.addEventListener("click", onLinkClick);
+
+function createGalleriItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onLinkClick(event) {
+  event.preventDefault();
+  getURLofLargeImage(event);
+  createModalWindov(event);
+}
+
+function getURLofLargeImage(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  console.log(event.target.dataset.source);
+}
+
+function createModalWindov(event) {
+  let imgLargeUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${imgLargeUrl}" width="1280" height="600">
+`,
+      {
+          onShow,
+        
+  });
+    instance.show();
+
+    function onShow(instance){
+        window.addEventListener('keydown', onEscKeyPress);
+    }
+    
+    function onEscKeyPress(event) {
+
+        if (event.code === "Escape") {
+            instance.close();
+            window.removeEventListener("keydown", onEscKeyPress); 
+        }
+    }
+  }
